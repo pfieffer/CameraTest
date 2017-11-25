62d74239170b466d1b5b1aa5cb2d654f7dccf2b2
RaviGarbujaPun <cbpmgr@gmail.com>
2017-11-25 15:59:29 +0545

    Implement Runtime Permissions, launch camera and get captured image

--- app/src/main/java/np/com/ravi/cameratest/MainActivity.java
+++ app/src/main/java/np/com/ravi/cameratest/MainActivity.java
@@ -1,13 +1,138 @@
 package np.com.ravi.cameratest;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private Button openDefCameraButton;
+    private ImageView imageFromCam;
+
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+    private View mLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = findViewById(R.id.main_layout);
+
+        openDefCameraButton = (Button) findViewById(R.id.button_def_camera);
+        imageFromCam = (ImageView) findViewById(R.id.imageView);
+
+        openDefCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCameraPreview();
+            }
+        });
     }
+
+    private void showCameraPreview() {
+        // Check if the Camera permission has been granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            Toast.makeText(MainActivity.this, "Starting Camera", Toast.LENGTH_SHORT).show();
+            startCamera();
+        } else {
+            // Permission is missing and must be requested.
+            requestCameraPermission();
+        }
+        // END_INCLUDE(startCamera)
+    }
+
+    private void startCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, PERMISSION_REQUEST_CAMERA);
+    }
+
+    private void requestCameraPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar.make(mLayout, "Camera access is required to display the camera preview.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+        }
+
+//        } else {
+//            Snackbar.make(mLayout,
+//                    "Permission is not available. Requesting camera permission.",
+//                    Snackbar.LENGTH_SHORT).show();
+//            // Request the permission. The result will be received in onRequestPermissionResult().
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+//                    PERMISSION_REQUEST_CAMERA);
+//        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // camera-related task you need to do.
+                    startCamera();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Snackbar.make(mLayout,
+                            "Permission is not available.",
+                            Snackbar.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //getting the image captured from camera
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        imageFromCam.setImageBitmap(thumbnail);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
